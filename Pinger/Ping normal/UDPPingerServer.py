cf17c129a1a8e6713aa8422b806f3db26d79064f
felipebfava <129697196+felipebfava@users.noreply.github.com>
2025-11-12 10:19:42 -0300

    ping normal server - atual

--- Pinger/Ping normal/UDPPingerServer.py
+++ Pinger/Ping normal/UDPPingerServer.py
@@ -1,24 +1,26 @@
+# código professora - lab UDPPinger
+
 # importando bibliotecas
 import random
 import socket
 
-# Cria um socket UDP
+# Cria um socket UDP usando IPv4, o mesmo que o nosso cliente
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Atribuir endereço IP e número da porta ao socket
-# Usaremos a porta 12000, aceitando requisições de qualquer IP (host)
+# Usaremos a porta 12000, aceitando requisições de qualquer IP (host) - 0.0.0.0
 serverSocket.bind(('', 12000))
 
 # Loop principal
 while True:
-    # Gerar número aleatório entre 0 e 10
+    # Gerar número aleatório entre 0 e 10 para simular perda de pacotes
     rand = random.randint(0, 10)
     
-    # Receber o pacote do cliente junto com o endereço de origem
+    # Recebe o pacote/mensagem do cliente junto com o endereço de origem
     # pacotes terão o tamanho máximo de 1024 bytes
     message, address = serverSocket.recvfrom(1024)
     
-    # Coloca a mensagem recebida em maiúscula
+    # Coloca a mensagem recebida em maiúscula para distinguir melhor entre mensagem cliente e mensagem servidor
     message = message.upper()
     
     # Se o número rand for menor que 4, consideramos que o pacote foi perdido e não respondemos
